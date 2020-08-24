f9a605c63a7757c8fb02a704d8936d893557e634
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-24 21:34:07 +0700

    Add menu

--- app/src/main/java/com/example/newfirstapp/TitleFragment.kt
+++ app/src/main/java/com/example/newfirstapp/TitleFragment.kt
@@ -1,11 +1,11 @@
 package com.example.newfirstapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.newfirstapp.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -13,6 +13,16 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
             R.layout.fragment_title,container,false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
