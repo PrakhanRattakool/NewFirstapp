76f711bb5b8962c9572f07a16c18dfa55930c952
Prakhan Rattakool <prakhan.ra@rmuti.ac.th>
2020-08-26 01:28:55 +0700

    Add database

--- app/src/main/java/com/example/newfirstapp/db/User.kt
+++ app/src/main/java/com/example/newfirstapp/db/User.kt
@@ -1,4 +1,10 @@
 package com.example.newfirstapp.db
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 class User {
+    @Entity(tableName = "User")
+    class Word(@PrimaryKey @ColumnInfo(name = "User") val word: String)
 }
